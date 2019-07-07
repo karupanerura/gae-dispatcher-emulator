88c1d774b7694ffb54910c3e6f60fcb4f993df06
karupanerura <karupa@cpan.org>
2019-07-07 20:09:56 +0900

    fix for lint

--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package gaedispemu
 
-//go:generate bash -c "cat VERSION | xargs -n1 printf 'package gaedispemu\n\nconst Version = \"%s\"\n' > version_gen.go"
+//go:generate bash -c "cat VERSION | xargs -n1 printf 'package gaedispemu\n\n// Version is a current version of the package.\nconst Version = \"%s\"\n' > version_gen.go"
--- version_gen.go
+++ version_gen.go
@@ -1,3 +1,4 @@
 package gaedispemu
 
+// Version is a current version of the package.
 const Version = "0.1.0"
